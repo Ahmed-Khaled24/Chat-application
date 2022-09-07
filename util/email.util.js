5d90e5ce3838592447471d12d54d9865dd8c534c
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-07 12:50:55 +0200

    feat(util): add email util file
    email util file uses Verifila API to validate email addresses

---
+++ util/email.util.js
@@ -0,0 +1,24 @@
+const { VerifaliaRestClient } = require('verifalia');
+
+const verifalia = new VerifaliaRestClient({
+    username: process.env.VERIFALIA_USERNAME,
+    password: process.env.VERIFALIA_PASSWORD,
+});
+
+async function validateEmail(email){
+    try{
+        const validation = await verifalia
+            .emailValidations
+            .submit(email, true);
+        return {
+            status: validation.entires[0].status.toLowerCase(),
+            classification: validation.entries[0].classification.toLowerCase(),
+        }      
+    } catch(err){
+        throw err;
+    }
+}
+
+module.exports = {
+    validateEmail,
+}

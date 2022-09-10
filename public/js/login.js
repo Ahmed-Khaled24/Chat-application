532be0111b3ca595d233977373d1d0d30e03f192
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-11 00:00:17 +0200

    refactor: login code

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,15 @@
 document.getElementById('login-form').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Stop the normal submit.
-    Array.from(document.getElementsByClassName('error')).forEach( (element)=> element.remove()); // Remove all the errors in the DOM
+    // Stop the normal submit action.
+    event.preventDefault(); 
+    // Remove all the errors in the DOM
+    Array.from(document.getElementsByClassName('error')).forEach( (element)=> element.remove()); 
 
     const email = document.getElementById('email').value.toLowerCase();
     const password = document.getElementById('password').value.toLowerCase();
     try {
-        let response = await fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email, password}),   
-        });
-        response = await response.text();
-        if(response.toLowerCase() === 'unauthorized'){
-            throw Error('Invalid email or password');
-        } else {
-            window.location.href = '/chat-room';
-        }
+        await sendLoginRequest(email, password);
     } catch(err){
-        const errorMessage = constructErrorMessage(err.message.toLowerCase()); 
-        const errorNode = createErrorNode(errorMessage);
-        return document.getElementById('login-form').appendChild(errorNode);
+        handleError(err, 'login-form');
     }
 });
 

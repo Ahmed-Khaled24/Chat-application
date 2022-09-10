f5e07606e5510716fa950f056f16eaed784a5929
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-11 00:10:06 +0200

    feat: create signup page with its all files

---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+document.getElementById('signup-form').addEventListener('submit', async (event) => {
+    // Stop the normal submit action.
+    event.preventDefault(); 
+    // Remove all the errors in the DOM
+    Array.from(document.getElementsByClassName('error')).forEach( (element)=> element.remove() ); 
+    
+    const firstName =  document.getElementById('firstName').value;
+    const lastName =  document.getElementById('lastName').value;
+    const username =  document.getElementById('username').value;
+    const email =  document.getElementById('email').value.toLowerCase();
+    const password =  document.getElementById('password').value;
+    try{
+        await sendSignupRequest(firstName, lastName, username, email, password);
+        await sendLoginRequest(email, password);
+        window.location.href = '/chat-room';
+    } catch (err){
+        handleError(err, 'signup-form');
+    }
+})
+

5f70d63e72a2d5ba6e6b9a998ddf4ee0a583ffbc
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-11 00:01:03 +0200

    feat: add requests.js file
    requests.js file contains two functions sendLoginRequest and
    sendSignupRequest

---
+++ public/js/requests.js
@@ -0,0 +1,35 @@
+async function sendLoginRequest(email, password){
+    let response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password}),   
+    });
+    response = await response.text();
+    if(response.toLowerCase() === 'unauthorized'){
+        throw Error('Invalid email or password');
+    } else {
+        window.location.href = '/chat-room';
+    }  
+}
+
+
+async function sendSignupRequest(firstName, lastName, username, email, password){
+    const isStrongPassword = checkStrength(password);
+    if(!isStrongPassword) {   
+        throw Error('Weak password, your password must be at least 8 characters, containing uppercase, lowercase, and digits.')  
+    }
+
+    let response = await fetch('/users', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({firstName, lastName, username, email, password}),   
+    });
+    response = await response.json();
+    if(response.status === 'fail'){
+        throw Error(response.error);
+    }
+}

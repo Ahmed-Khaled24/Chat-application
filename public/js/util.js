484d81f6d7ea6e2b4756ab6f382e0c0c2562cddf
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-13 00:25:19 +0200

    feat: add simplifyDate to frontend util.js

--- public/js/util.js
+++ public/js/util.js
@@ -31,3 +31,46 @@ function checkStrength(password){
         return true; // TODO: implement lowercase uppercase digits check
     }
 }
+
+function simplifyDate(messageDate) {
+	const monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	const createdAt = messageDate.valueOf();
+	const renderAt = Date.now();
+
+	const minutes = Math.floor((renderAt - createdAt) / (1000 * 60));
+	if (minutes < 1) {
+		return 'Now';
+	}
+	else if (minutes == 1) {
+		return `${minutes} minute ago`;
+	}
+
+	const hours = Math.floor(minutes / 60);
+	if (hours < 1) {
+		return `${minutes} minutes ago`;
+	}
+	else if (hours == 1) {
+		return `${hours} hour ago`;
+	}
+	else if (hours > 1 && hours < 24) {
+		return `${hours} hours ago`;
+	}
+
+	const days = Math.floor(hours / 24);
+	if (days == 1) {
+		return `${days} day ago`;
+	}
+	else if (days < 30) {
+		return `${days} days ago`;
+	}
+
+	const months = Math.floor(days / 30);
+	if (months == 1) {
+		return `${months} month ago`
+	}
+	else if (months < 12) {
+		return `${months} months ago`
+	}
+
+	return `${monthsName[messageDate.getMonth()]}, ${messageDate.getFullYear()}`
+}

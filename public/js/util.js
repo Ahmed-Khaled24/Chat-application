0b9e9a7fb9ff779ed1d0ba74bc97a3918ef85ba7
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-15 11:56:21 +0200

    fix: typo in frontend util.js

--- public/js/util.js
+++ public/js/util.js
@@ -8,7 +8,7 @@ function createErrorNode(errorTxt){
 
 function constructErrorMessage(responseError){
     if(responseError.includes('duplicate') && responseError.includes('email')){
-        return `The email you entered is already registered use it to login or user another email`
+        return `The email you entered is already registered use it to login or use another email`
     } else if (responseError.includes('duplicate') && responseError.includes('username')){
         return `The username you entered is already taken, choose another one`
     } else if(responseError === 'failed to fetch') {
